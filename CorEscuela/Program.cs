a9dd8e056d2f302ffacb3e27d3fea675451522d4
Hernan Pinos <hernan.pinos@outlook.com>
2020-03-12 18:24:40 -0500

    CREACION DE LA ESTRUCTURA DEL PROYECTO CORESCUELA

---
+++ CorEscuela/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CorEscuela
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
