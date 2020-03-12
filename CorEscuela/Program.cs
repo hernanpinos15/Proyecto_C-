7f431ca6a9238e8710cedd471f134f09c09dd15a
Hernan Pinos <hernan.pinos@outlook.com>
2020-03-12 18:41:37 -0500

    Creacion de la clase escuela y su funcion timbrar

--- CorEscuela/Program.cs
+++ CorEscuela/Program.cs
@@ -2,11 +2,29 @@
 
 namespace CorEscuela
 {
+    class Escuela
+    {
+        public string nombre;
+        public string direccion;
+        public int añoFundacion;
+        public string ceo;
+
+        public void Timbrar()
+        {
+            Console.Beep(10000, 3000);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var miEscuela = new Escuela();
+            miEscuela.nombre = "Hernan Academy";
+            miEscuela.direccion = "Cr 9 calle 72";
+            miEscuela.añoFundacion = 2012;
+            Console.WriteLine("Timbre");
+            miEscuela.Timbrar();
+            //Console.WriteLine("Hello World!");
         }
     }
 }
